9ecbc226d589c081a88b24ebf7689effc419e3b9
mohdarshad1 <mdarshad181@gmail.com>
2021-05-24 17:31:07 +0530

    [Mohd Arshad] : Calculate Wage of Month

--- empWage.js
+++ empWage.js
@@ -4,7 +4,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-
+const NUM_OF_WORKING_DAYS = 20;
 function getWorkHours(empCheck){
 switch(empCheck){
     case IS_PART_TIME:
@@ -15,7 +15,10 @@ switch(empCheck){
         return 0;
 }
 }
-empCheck = Math.floor(Math.random()*10)%3;
-let empHrs = getWorkHours(empCheck);
+let empHrs = 0;
+for(let day =0;day<NUM_OF_WORKING_DAYS;day++){
+    empCheck = Math.floor(Math.random()*10)%3;
+    empHrs += getWorkHours(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage: "+empWage);
