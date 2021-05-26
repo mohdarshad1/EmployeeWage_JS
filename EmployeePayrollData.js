7ad361da94965063136561d98a02de0dff985d96
mohdarshad1 <mdarshad181@gmail.com>
2021-05-26 17:59:18 +0530

    [Mohd Arshad] : Employee payroll Data Regex

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -13,7 +13,12 @@ class EmployeePayrollData{
     }
 
     get name(){ return this._name;}
-    set name(name){this._name = name;}
+    set name(name){
+        let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+        if(nameRegex.test(name))
+            this._name = name;
+        else throw "Name is Incorrect";
+    }
 
     toString(){
         const options = { year: 'numeric', month: 'long', day: 'numeric'};
@@ -26,7 +31,11 @@ class EmployeePayrollData{
 
 let employeePayrollData = new EmployeePayrollData(1,"Mark",30000);
 console.log(employeePayrollData.toString());
+try{
 employeePayrollData.name = "john";
 console.log(employeePayrollData.toString());
+}catch(e){
+    console.error(e);
+}
 let newEmployeePayrollData = new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
 console.log(newEmployeePayrollData);
